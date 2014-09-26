6d1835a5121cd639e305e7f761101ba4994a0372
[cdonohue1] <[chasedonohue01@gmail.com]>
2014-09-26 08:54:34 -0600

    added views for edit index and show for shops

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -18,16 +18,20 @@ class ShopsControllerTest < ActionController::TestCase
 
   test "should get create" do
     get :create
+    shop :create, shop: { name: @shop.name, discription: @shop.description website: @shop.website}
+    assert_redirected_to shop_path(assigns(:shop))
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @post
     assert_response :success
   end
 
   test "should get update" do
     get :update
+    patch :update, id: @shop, shop: { name: @shop.name, discription: @shop.description website: @shop.website }
+    assert_redirected_to shop_path(assigns(:shop))
     assert_response :success
   end
 
