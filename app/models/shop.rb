0762c366873f08a23404fc98b1a6983004f653c3
[pwalker86] <[phil1586@gmail.com]>
2014-09-30 15:57:58 -0600

    Added associations with favorites

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,7 @@
 class Shop < ActiveRecord::Base
   has_many :drinks
   has_many :reviews, through: :drinks
+  has_many :favorites, as: :favorable
     
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
@@ -9,4 +10,5 @@ class Shop < ActiveRecord::Base
       :name
     ]
   end
+
 end
