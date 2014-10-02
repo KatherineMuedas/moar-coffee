80a07a3cfd51397b3546f7486e05c3263c35a424
[pwalker86] <[phil1586@gmail.com]>
2014-10-02 09:30:54 -0600

    Displaying favorites on user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_many :reviews
+  has_many :favorites, as: :favorable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -18,7 +19,7 @@ class User < ActiveRecord::Base
     ]
   end
 
-  def favorites
+  def favorited_shops
     favorites.collect { |x| x.favorable }
   end
 
