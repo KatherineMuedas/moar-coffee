de3537db3f0a074d748209c0e8a7e6bcc4b10b37
katherine <muedasnka@gmail.com>
2014-10-06 01:09:29 -0600

    cherrypick navbar

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
   belongs_to :picable, polymorphic: true
   belongs_to :user
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
