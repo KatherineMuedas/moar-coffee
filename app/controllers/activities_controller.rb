8813b6de86abdc202c8d0ca81abbda0593a3b762
[pwalker86] <[phil1586@gmail.com]>
2014-10-08 15:21:07 -0600

    added own id to news feed list

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
   def index
     @ids = current_user.all_following.map(&:id)
+    @ids << current_user.id
     @activities = PublicActivity::Activity.order("created_at DESC").where(follow_id: @ids)
   end
 end
