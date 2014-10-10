49424015749099e121dc580ae72fc4dfb36840cb
[pwalker86] <[phil1586@gmail.com]>
2014-10-10 14:26:48 -0600

    removed own activities from feed.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
     @ids = current_user.all_following.map(&:id)
-    @ids << current_user.id
     @activities = PublicActivity::Activity.order("created_at DESC").where(follow_id: @ids)
   end
 end
