978e1fd3991a9f6cefb4cc49b864a180f00683cf
[cdonohue1] <[chasedonohue01@gmail.com]>
2014-10-10 14:55:39 -0600

    changed routes for ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
 
   resources :activities
   resources :favorites, only: [:create, :destroy]
@@ -10,8 +10,10 @@ Rails.application.routes.draw do
 
   resources :shops, except: [:destroy] do
     resources :drinks, except: [:index, :destroy]
-    resources :follows, only: [:create, :destroy]
+    resources :follows, only: [:create] do
+    end
   end
+  delete 'shops/:shop_id/follows' => 'follows#destroy', as: 'shop_follows_delete'
   
   resources :drinks, only: [] do
     resources :reviews, only: [:create, :destroy]
