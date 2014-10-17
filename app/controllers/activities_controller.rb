65560cb4d15fcb6fe02ffc1027087ff26708769d
[cdonohue1] <[chasedonohue01@gmail.com]>
2014-10-17 11:09:44 -0600

    added reek gem and some comments, as well as fixed sign in modal

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
+    # adds follow id to ids
     @ids = current_user.all_following.map(&:id)
     @activities = PublicActivity::Activity.order("created_at DESC").where(follow_id: @ids)
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    # sanatizing devise inputs
     devise_parameter_sanitizer.for(:sign_up) << [:name, :zipcode, :gender, :avatar]
     devise_parameter_sanitizer.for(:account_update) << [:name, :zipcode, :gender, :avatar]
   end
